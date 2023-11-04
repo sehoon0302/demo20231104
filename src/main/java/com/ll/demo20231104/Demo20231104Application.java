fa3cae54c49d9999fbbb455788af807f263f1989
sehoon0302 <saehun66@gmail.com>
2023-11-04 11:53:52 +0900

    Initial commit

---
+++ src/main/java/com/ll/demo20231104/Demo20231104Application.java
@@ -0,0 +1,13 @@
+package com.ll.demo20231104;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo20231104Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo20231104Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/demo20231104/Demo20231104ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.demo20231104;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo20231104ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
